6988c0e6bb4b6038476fa850ad64fa2edc2f914b
jorgeortega123 <garridomateo74@gmail.com>
2025-05-17 19:23:18 -0500

    Titulo de pagina agregado

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,14 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
-
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <title>CountDowns | Conteos regresivos online </title>
+        <meta
+          name="description"
+          content="Agrega conteos regresivos desde 1 minuto al futuro hasta que el fin de los tiempos"
+        />
         <script src="https://cdn.tailwindcss.com"></script>
       </Head>
       <body className="dark">
